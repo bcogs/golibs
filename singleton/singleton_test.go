ea9578fa3303c39b2490f6af373219f9e995818a
Brieuc Jeunhomme <brieuc.jeunhomme@paymentsense.com>
2022-12-20 20:26:19 +0000

    replace oil pairs with a local implem because go mod version management sucks

--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -8,7 +8,6 @@ import (
 
 	"github.com/stretchr/testify/assert"
 
-	"github.com/bcogs/golibs/oil"
 	"github.com/bcogs/golibs/singleton"
 )
 
@@ -57,6 +56,13 @@ func (c *createlog) all() []int {
 	}
 }
 
+type pair struct {
+	s string
+	e error
+}
+
+func newPair(s string, e error) pair { return pair{s, e} }
+
 func TestSingletonBasics(t *testing.T) {
 	t.Parallel()
 	var s singleton.Singleton[int]
@@ -97,12 +103,12 @@ func TestSingletonMapBasics(t *testing.T) {
 	assert.Equal(t, "1", sm.GetOrCreate(1, createlog.createWithKey))
 	assert.Equal(t, "2", sm.GetOrCreate(2, createlog.createWithKey))
 	assert.Equal(t, "1", sm.GetOrCreate(1, createlog.createWithKey))
-	assert.Equal(t, oil.NewPair("3", error(nil)), oil.NewPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndSucceed)))
+	assert.Equal(t, newPair("3", error(nil)), newPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndSucceed)))
 	_, err := sm.GetOrCreateOrFail(4, createlog.createWithKeyAndFail)
 	assert.Error(t, err)
-	assert.Equal(t, oil.NewPair("4", error(nil)), oil.NewPair(sm.GetOrCreateOrFail(4, createlog.createWithKeyAndSucceed)))
-	assert.Equal(t, oil.NewPair("3", error(nil)), oil.NewPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndSucceed)))
-	assert.Equal(t, oil.NewPair("3", error(nil)), oil.NewPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndFail)))
+	assert.Equal(t, newPair("4", error(nil)), newPair(sm.GetOrCreateOrFail(4, createlog.createWithKeyAndSucceed)))
+	assert.Equal(t, newPair("3", error(nil)), newPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndSucceed)))
+	assert.Equal(t, newPair("3", error(nil)), newPair(sm.GetOrCreateOrFail(3, createlog.createWithKeyAndFail)))
 	assert.Equal(t, createlog.all(), []int{1, 2, 3, -4, 4})
 }
 
@@ -131,7 +137,7 @@ func TestSingletonMapRaces(t *testing.T) {
 				case 0:
 					assert.Equal(t, s, sm.GetOrCreate(i, createlog.createWithKey))
 				case 1:
-					assert.Equal(t, oil.NewPair(s, error(nil)), oil.NewPair(sm.GetOrCreateOrFail(i, createlog.createWithKeyAndSucceed)))
+					assert.Equal(t, newPair(s, error(nil)), newPair(sm.GetOrCreateOrFail(i, createlog.createWithKeyAndSucceed)))
 				}
 				wg.Done()
 			}(i, s)
@@ -140,7 +146,7 @@ func TestSingletonMapRaces(t *testing.T) {
 	close(leash)
 	wg.Wait()
 	for i := 1; i <= P; i++ {
-		assert.Equal(t, oil.NewPair(strconv.Itoa(i), error(nil)), oil.NewPair(sm.GetOrCreateOrFail(i, createlog.createWithKeyAndFail)))
+		assert.Equal(t, newPair(strconv.Itoa(i), error(nil)), newPair(sm.GetOrCreateOrFail(i, createlog.createWithKeyAndFail)))
 	}
 	assert.ElementsMatch(t, expected, createlog.all())
 }
